ec3819e61931e70d22138a2c50bd4051eba1c0fc
Ekaterina Polehina <gorodnowa.kat@yandex.ru>
2023-06-23 13:50:50 +0300

    Создание классов исключений

--- Morskoi boi.py
+++ Morskoi boi.py
@@ -2,10 +2,21 @@ class Dot:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
     def __repr__(self):
         return f"Dot({self.x}, {self.y})"
+class BoardException(Exception):
+    pass
+
+class BoardOutException(BoardException):
+    def __str__(self):
+        return "Вы пытаетесь выстрелить за доску!"
+class BoardUserException(BoardException):
+    def __str__(self):
+        return "Вы уже стреляли в эту клетку!"
+class BoardWrongShipException(BoardException):
+    pass
+
 
