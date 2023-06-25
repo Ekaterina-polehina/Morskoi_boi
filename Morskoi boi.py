93929f2b22f074794e0aa6bd298eb2c9a36132b4
Ekaterina Polehina <gorodnowa.kat@yandex.ru>
2023-06-25 13:46:20 +0300

    Стрельба по доске

--- Morskoi boi.py
+++ Morskoi boi.py
@@ -1,23 +1,33 @@
 class BoardException(Exception):
     pass
 
+
 class BoardOutException(BoardException):
     def __str__(self):
         return "Вы пытаетесь выстрелить за доску!"
+
+
 class BoardUsedException(BoardException):
     def __str__(self):
         return "Вы уже стреляли в эту клетку!"
+
+
 class BoardWrongShipException(BoardException):
     pass
+
+
 class Dot:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
     def __repr__(self):
         return f"Dot({self.x}, {self.y})"
+
+
 class Ship:
     def __init__(self, bow, l, o):
         self.bow = bow
@@ -43,30 +53,32 @@ class Ship:
     def shooten(self, shot):
         return shot in self.dots
 
+
 class Board:
-    def __int__(self, hid = False, size = 6):
+    def __int__(self, hid=False, size=6):
         self.size = size
         self.hid = hid
 
         self.count = 0
 
-        self.field = [["o"]*size for _ in range(size)]
+        self.field = [["o"] * size for _ in range(size)]
 
         self.busy = []
         self.ships = []
+
     def __str__(self):
         res = ""
         res += "| 1 | 2 | 3 | 4 | 5 | 6 |"
         for i, row in enumerate(self.field):
-            res += f"\n{i+1} | "+" | ".join(row) + " | "
+            res += f"\n{i + 1} | " + " | ".join(row) + " | "
         if self.hid:
             res = res.replace("#", "o")
         return res
 
     def out(self, d):
-        return not((0 <= d.x < self.size) and (0 <= d.y < self.size))
+        return not ((0 <= d.x < self.size) and (0 <= d.y < self.size))
 
-    def contour(self, ship, verb = False):
+    def contour(self, ship, verb=False):
         near = [
             (-1, -1), (-1, 0), (-1, 1),
             (0, -1), (0, 0), (0, 1),
@@ -75,7 +87,7 @@ class Board:
         for d in ship.dots:
             for dx, dy in near:
                 cur = Dot(d.x + dx, d.y + dy)
-                if not(self.out(cur)) and cur not in self.busy:
+                if not (self.out(cur)) and cur not in self.busy:
                     if verb:
                         self.field[cur.x][cur.y] = "."
                     self.busy.append(cur)
@@ -91,6 +103,30 @@ class Board:
         self.ships.append(ship)
         self.contour(ship)
 
+    def shot(self, d):
+        if self.out(d):
+            raise BoardOutException()
 
+        if d in self.busy:
+            raise BoardUsedException()
 
+        self.busy.append(d)
 
+        for ship in self.ships:
+            if ship.shooten(d):
+                ship.lives -= 1
+                self.field[d.x][d.y] = "X"
+                if ship.lives == 0:
+                    self.count += 1
+                    self.contour(ship, verb=True)
+                    print("Корабль уничтожен!")
+                    return False
+                else:
+                    print("Корабль ранен!")
+                    return True
+        self.field[d.x][d.y] = "."
+        print("Мимо!")
+        return False
+
+    def begin(self):
+        self.busy = []
