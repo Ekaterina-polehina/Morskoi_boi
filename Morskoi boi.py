5f0532fa6c967ee209a4b83dbd11b6b156b1e080
Ekaterina Polehina <gorodnowa.kat@yandex.ru>
2023-06-25 11:49:45 +0300

    Иговое поле

--- Morskoi boi.py
+++ Morskoi boi.py
@@ -43,5 +43,28 @@ class Ship:
     def shooten(self, shot):
         return shot in self.dots
 
+class Board:
+    def __int__(self, hid = False, size = 6):
+        self.size = size
+        self.hid = hid
+
+        self.count = 0
+
+        self.field = [["o"]*size for _ in range(size)]
+
+        self.busy = []
+        self.ships = []
+    def __str__(self):
+        res = ""
+        res += "| 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, row in enumerate(self.field):
+            res += f"\n{i+1} | "+" | ".join(row) + " | "
+        if self.hid:
+            res = res.replace("#", "o")
+        return res
+
+    def out(self, d):
+        return not((0 <= d.x < self.size) and (0 <= d.y < self.size))
+
 
 
